b5b7f467024d35933dcccbf2b1b71ba044eef15a
Xi He <xh243@cornell.edu>
2014-11-12 18:23:47 -0500

    Initial Commit

---
+++ handSeg/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  handSeg
+//
+//  Created by Xi He on 14-11-12.
+//  Copyright (c) 2014年 Xi He. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
